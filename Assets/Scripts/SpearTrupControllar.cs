cf97e1ea6ec7f18a963db50d0179188a3c062b3c
asa-0580 <deepred409@yahoo.co.jp>
2016-05-28 21:59:33 +0900

    gimmick

--- Assets/Scripts/SpearTrupControllar.cs
+++ Assets/Scripts/SpearTrupControllar.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class SpearTrupControllar : MonoBehaviour {
 
+    private bool falg = false;
+
+    public int speed = 12;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,14 +14,19 @@ public class SpearTrupControllar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (falg == true && transform.position.y < 6)
+        {
+            transform.Translate(Vector3.up * (Time.deltaTime * speed) );
+        }
+
 	}
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-
+            //transform.Translate(Vector3.up * 5);
+            falg = true;
 
         }
     }
