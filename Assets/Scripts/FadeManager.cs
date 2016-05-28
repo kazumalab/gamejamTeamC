447ef7e5403c8d451f373f848cde4cad9802a067
Kazumalab <kazuma-arashi@hotmail.co.jp>
2016-05-28 19:38:35 +0900

    繰り返しできるように変更

---
+++ Assets/Scripts/FadeManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour {
+
+	private bool isFade = false;
+	private float count = 1f;
+
+
+	private bool isFastFade;
+	private bool isEndFade = false;
+
+	public Image FadeImage;
+
+	// Use this for initialization
+	void Start () {
+		isFade = true;
+		isFastFade = true;
+		setAlpha (FadeImage, count);
+	}
+
+	void Update () {
+		if (isFastFade) {
+			FadeIn (FadeImage);
+		}
+
+		if (isEndFade) {
+			FadeOut (FadeImage);
+		}
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "Player") {
+			isFade = true;
+			isEndFade = true;
+		}
+	}
+
+	void setAlpha(Image image,float alpha) {
+		image.color = new Color (image.color.r, image.color.g, image.color.b, alpha);
+	}
+
+	void FadeOut(Image image) {
+		if (isFade) {
+			count += 0.025f;
+			setAlpha (image, count);
+			if (image.color.a >= 1f) {
+				isFade = false;
+				if (isEndFade) {
+					SceneManager.LoadScene (0);
+				}
+			}
+		}
+	}
+
+	void FadeIn(Image image) {
+		if (isFade) {
+			count -= 0.025f;
+			setAlpha (image, count);
+			if (image.color.a <= 0f) {
+				isFade = false;
+				isFastFade = false;
+			}
+		}
+	}
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -35,7 +35,7 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 v3 = new Vector3 (0, 0, 0);
 
-		v3.y -= 10000f * Time.deltaTime;
+		v3.y -= 1000f * Time.deltaTime;
 
 		Vector2 mouseSpeed = new Vector2 (Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"));
 		PlayerCameraRotate (mouseSpeed);
@@ -54,7 +54,7 @@ public class PlayerController : MonoBehaviour {
 
 	void PlayerMove() {
 		float dx = Input.GetAxis ("Vertical");
-		heads.transform.position += heads.transform.TransformDirection (Vector3.forward) * dx;
+		heads.transform.position += heads.transform.TransformDirection (Vector3.forward) * dx * 2;
 	}
 
 	void OnTriggerEnter(Collider col) {
